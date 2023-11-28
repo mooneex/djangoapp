f32104e2a702283f44cb61b0615c9d1911e07c45
mooneex <erkimelzar@gmail.com>
2023-11-28 21:03:17 +0600

    adding requirements

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import unittest
+from unittest.mock import MagicMock
+from .management.commands.run_telegram_bot import start, games, handle_help, handle_add_game, echo_all
 
-# Create your tests here.
+
+class TestBot(unittest.TestCase):
+
+    def test_start(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        start(message)
+
+    def test_help(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        handle_help(message)
+
+    def test_figures(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        games(message)
+
+    def test_add_figure(self):
+        message = MagicMock()
+        message.chat.id = 534895748
+        handle_add_game(message)
+
+
+if __name__ == '__main__':
+    unittest.main()
